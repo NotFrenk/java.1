a29954225fa1bb1a6a00468418d5af550d68d4e1
NotFrenk <franciscoleonfelici@gmail.com>
2024-10-07 14:01:34 +0200

    esercizi java

---
+++ P20241002_3_pitagora/src/teorema_di_pitagora.java
@@ -0,0 +1,19 @@
+
+public class teorema_di_pitagora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Dato un triangolo rettangolo i cui cateti misurano rispettivamente 45.3 e 67.2 metri,
+		 * 1) calcolare e stampare la lunghezza dell'ipotenusa
+		 * 2) calcolare e stampare la sua area
+		 * 3) calcolare e stampare il suo perimetro
+		 */
+		
+		double cateto1 = 45.3;
+		double cateto2 = 67.2;
+		
+		double 
+	}
+
+}
---
+++ Prima volta in java/src/Primo.java
@@ -0,0 +1,14 @@
+
+public class Primo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*gjgtgoigjgoijgotjgei*/
+		
+		// le istruzioni terminano con ;
+		
+		System.out.println("ciao come stai?");
+
+	}
+
+}
